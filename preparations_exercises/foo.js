1e69b9517b9827f8889846dc95c7167c18d2adbb
Stephen Panasuik <panasuiks@gmail.com>
2021-10-22 13:52:50 -0400

    First preparation commit

---
+++ preparations_exercises/foo.js
@@ -0,0 +1,12 @@
+let foo = 'bar';
+console.log(foo);
+foo;
+let x = '5';
+let y = Number(x);
+console.log(typeof (x) + typeof (y));
+
+x = parseFloat('12.5foo');
+console.log(Infinity);
+console.log(x);
+console.log();
+
